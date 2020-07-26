5618c234ceb501341c40074bc259bcf25e8c11da
Bngzifei <bngzifei@gmail.com>
2020-07-26 22:44:21 +0800

    [FIX]添加抵押的项目资料

---
+++ 新项目资料.py
@@ -0,0 +1,43 @@
+https://github.com/MPOS/php-mpos
+
+https://rahulmanuwas.gitbooks.io/mpos-nomp/content/
+
+https://beam.suprnova.cc/
+
+https://www.bfcpool.com/account/profit.html
+
+账户54303771@qq.com 密码w123456
+
+
+数据库没有密码:
+登录账号是mysql
+直接mysql -umysql就可以进去.
+
+ubuntu18.04卸载mysql:
+https://blog.csdn.net/iehadoop/article/details/82961264
+
+ubuntu18安装mysql教程:
+https://blog.csdn.net/dmedaa/article/details/89965556
+
+您新购买云服务器:
+154.221.25.18
+账号：root
+密码：3vyQ725M
+
+ubuntu18源码安装mysql5.6:
+https://blog.csdn.net/kevinhades/article/details/88692551
+
+
+https://blog.csdn.net/kevinhades/article/details/88692551
+
+/usr/local/mysql/scripts/mysql_install_db --user=mysql --basedir=/usr/local/mysql --datadir=/data/mysqldata
+
+
+python2.7问题记录:
+https://blog.csdn.net/wangjiegege/article/details/77478870
+
+navicat导入sql文件:
+https://www.cnblogs.com/chenlimei/p/10375676.html
+
+
+
